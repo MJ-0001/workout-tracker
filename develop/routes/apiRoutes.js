38819b92304001a1b64d218974e389b983d8720d
Matthew Jones <mejones92@gmail.com>
2021-04-24 17:59:46 +0100

    Added routes to perform CRUD operations on mongodb.

---
+++ develop/routes/apiRoutes.js
@@ -0,0 +1,59 @@
+const router = require('express').Router();
+const { Workout } = require('../models');
+
+router.post('/', async ({ body }, res) => {
+  Workout.create({ body })
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+router.put('/:id', async ({ body, params }, res) => {
+  Workout.findByIdAndUpdate(params.id, { $push: { exercises: body } })
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+router.get('/', (req, res) => {
+  Workout.aggregate([{
+    $addFields: {
+      totalDuration: {
+        $sum: '$exercises.duration'
+      }
+    }
+  }])
+  .sort({ date: -1 })
+  .then(dbWorkout => {
+    res.json(dbWorkout);
+  })
+  .catch(err => {
+    res.status(400).json(err);
+  });
+});
+
+router.get('/range', (req, res) => {
+  Workout.aggregate([{
+    $addFields: {
+      totalDuration: {
+        $sum: '$exercises.duration'
+      }
+    }
+  }])
+  .sort({ date: -1 })
+  .limit(7)
+  .then(dbWorkout => {
+    res.json(dbWorkout);
+  })
+  .catch(err => {
+    res.status(400).json(err);
+  });
+});
+
+module.exports = router;
